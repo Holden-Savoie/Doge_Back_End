a31ebd2a32f3d8dd715ef8aeb1018b85de63dd39
Taylor Cyr <michaelcyr79@yahoo.com>
2021-05-16 21:59:36 -0500

    Added Cors, & timer

--- scrapers.js
+++ scrapers.js
@@ -1,18 +1,38 @@
 const puppeteer = require('puppeteer-extra');
 const StealthPlugin = require('puppeteer-extra-plugin-stealth')
 puppeteer.use(StealthPlugin())
+var price = 0.0000;
+var loggedDate = Date.now();//vars only run once on startup
 
-async function scrapePrice(url){
+const express = require('express') //webserver
+const app = express()
+const path = require('path');
+const cors = require('cors');
+app.use(cors())
+ 
+app.get('/dogePrice', async (req, res) => {
+    try{
+        let currentDate = Date.now();
+        //If we've done this <10 secs ago, just return old number - save power
+        if((currentDate-loggedDate) < 10000){
+            res.status(200).send(JSON.stringify({"price":price})); 
+            return;
+        }
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    await page.goto(url, { waitUntil: 'domcontentloaded' });
+    await page.goto('https://cryptowat.ch/charts/KRAKEN:DOGE-USD', { waitUntil: 'domcontentloaded' });
     await page.waitForSelector('#price-ticker');
     const [el] = await page.$x('//*[@id="price-ticker"]')//xpath here
     const txt = await (await el.getProperty('textContent')).jsonValue();
 
-    console.log([txt]);
+    price = parseFloat(txt.substring(0, 6)).toFixed(4);
+    res.status(200).send(JSON.stringify({"price":price}))
+    await browser.close();
+    loggedDate = currentDate;
+    }
+    catch(err){
+        res.status(500).send(JSON.stringify({"error":err})) 
+    }
+})
 
-    browser.close();
-}
-
-scrapePrice('https://cryptowat.ch/charts/KRAKEN:DOGE-USD');
+app.listen(5000)
